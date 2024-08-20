4ed7ff792ef4f189cd3344114538c87c64d7b8c1
h <haavard_martin@hotmail.com>
2024-08-20 20:40:06 +0200

    1.1.5

--- pages/content/index.vue
+++ pages/content/index.vue
@@ -7,7 +7,15 @@ export default {
   data() {
     return {
       selectedCategory: {name:"Select a topic", path: null},
-      categories: REPLACE_WITH_FOLDER_INFO,
+      categories: [
+        {name:"checklists", path: "blog/checklists"},
+        {name:"engineering", path: "blog/engineering"},
+        {name:"philosophy", path: "blog/philosophy"},
+        {name:"tutorials", path: "blog/tutorials"},
+        {name:"trading", path: "blog/trading"},
+        {name:"music", path: "blog/music"},
+        {name:"life", path: "blog/life"},
+      ],
     }
   },
 
