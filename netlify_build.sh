85ccb82d3657be5f4c7487affa95c9141928a0de
h <haavard_martin@hotmail.com>
2024-08-20 20:23:16 +0200

    1.1.1

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
 
+folders=$(find content/blog/ -mindepth 1 -maxdepth 1 -type d)
 
-blog_categories="["
-
-folders=$(find blog -mindepth 1 -maxdepth 1 -type d)
+json_array="["
 for folder in $folders; do
     name=$(basename "$folder")
     path="$folder"
-    blog_categories+="{\"name\":\"$name\", \"path\":\"$path\"},"
+    json_array+="{\"name\":\"$name\", \"path\":\"blog/$name\"},"
 done
+json_array+="]"
 
-blog_categories+="]"
-
-echo "$blog_categories"
-
-git clone https://github.com/kristus123/bloggy.git
-
-rm -r bloggy/content/blog/
-cp -r blog/ bloggy/content/
-
-rm -r bloggy/static/public/
-cp -r public/ bloggy/static/
-
-# Delete the 'home button'
-sed -i '/<center>/,/<\/center>/{//!d;}' bloggy/layouts/cv.vue
+echo "$json_array"
 
 
-input_file="bloggy/pages/content/index.vue"
-sed -i "s|REPLACE_WITH_FOLDER_INFO|$blog_categories|" "$input_file"
 
+input_file="pages/content/index.vue"
+original_content=$(cat "$input_file")
+sed -i "s|REPLACE_WITH_FOLDER_INFO|$json_array|" "$input_file"
 
-cd bloggy
 npm install
 export NODE_OPTIONS=--openssl-legacy-provider
 npm run generate
 
+echo "$original_content" > "$input_file"
