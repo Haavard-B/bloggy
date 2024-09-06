bdc884c7b685e576081a10b55d854fd43bdae0b5
h <haavard_martin@hotmail.com>
2024-09-07 00:05:49 +0200

    1.3.2

--- components/content/Card.vue
+++ components/content/Card.vue
@@ -3,7 +3,7 @@
       <v-card class="motion pa-4" width="350">
           <h2>{{ page.title }}</h2>
           <p class="mt-4">{{ page.description }}</p>
-          <p v-if="page.date" class="text-grey-500">{{ page.date }}</p>
+          <p v-if="page.date" class="mt-4">{{ page.date }}</p>
           <div class="mt-5 mb-4"></div>
 
           <v-img
