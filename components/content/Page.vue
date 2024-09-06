6a9594576300a9517f895f13a7f62e8151b02ff8
h <haavard_martin@hotmail.com>
2024-09-06 23:18:16 +0200

    1.2.37

--- components/content/Page.vue
+++ components/content/Page.vue
@@ -86,6 +86,7 @@ export default {
             .only([
               "title",
               "description",
+              "date",
               "pathPrefix",
               "slug",
               "coverImage",
--- components/cv/Projects.vue
+++ components/cv/Projects.vue
@@ -24,6 +24,7 @@ export default {
   props: [
     "title",
     "description",
+    "date",
     "imageUrl",
     "technologyUsed",
     "amountOfMonths",
